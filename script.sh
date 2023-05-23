33ce109a238e55226e2b07da7e8d94cf9111037c
Bernadetten <56475705+Bernadetten@users.noreply.github.com>
2023-05-23 18:59:56 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ filename="banner.txt"
 # Read and print the file line by line
 while IFS= read -r line; do
     echo "$line"
-    sleep 1 # Delay of 1ms
+    sleep 0.05
 done < "$filename"
 
 echo # Print a new line at the end
