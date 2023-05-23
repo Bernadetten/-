8bd0a956f32b415df36062182ce303255a96ecf4
Bernadetten <56475705+Bernadetten@users.noreply.github.com>
2023-05-23 18:37:45 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,7 @@ filename="banner.txt"
 # Read and print the file character by character
 while IFS= read -r -n1 char; do
     printf "%s" "$char"
+    sleep 0.001 # Delay of 1ms
 done < "$filename"
 
 echo # Print a new line at the end
